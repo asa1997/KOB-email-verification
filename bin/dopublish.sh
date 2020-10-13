3dd34bdeda58cd65b9e8e06b294b68687e477b79
asa1997 <arunsureshampadath@gmail.com>
2020-10-13 14:44:56 +0530

    Excluding tmpl files

--- bin/dopublish.sh
+++ bin/dopublish.sh
@@ -10,7 +10,7 @@ git checkout $branch
 
 mkdir -p build/tmp
 
-zip -r $HOME/KOB-email-verification/build/tmp/kob-email-latest.zip COMPLIANCE.yaml $(ls -d */ | grep -v "bin" | grep -v "build") 
+zip -r $HOME/KOB-email-verification/build/tmp/kob-email-latest.zip COMPLIANCE.yaml $(ls -d */ | grep -v "bin" | grep -v "build") -x scripts/tmpl scripts/*.io
 
 cp $HOME/KOB-email-verification/build/tmp/kob-email-latest.zip $HOME/KOB-email-verification/build/tmp/kob-email-$version.zip
 
