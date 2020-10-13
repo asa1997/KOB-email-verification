d7cbcdeaedf31e5cd8459c3340f721b46b97bf9c
asa1997 <arunsureshampadath@gmail.com>
2020-10-13 14:13:12 +0530

    Cleared name warning for Compliance.yaml

--- bin/dopublish.sh
+++ bin/dopublish.sh
@@ -10,7 +10,7 @@ git checkout $branch
 
 mkdir -p build/tmp
 
-zip -r $HOME/KOB-email-verification/build/tmp/kob-email-latest.zip COMPLIANCE.yml $(ls -d */ | grep -v "bin" | grep -v "build") 
+zip -r $HOME/KOB-email-verification/build/tmp/kob-email-latest.zip COMPLIANCE.yaml $(ls -d */ | grep -v "bin" | grep -v "build") 
 
 cp $HOME/KOB-email-verification/build/tmp/kob-email-latest.zip $HOME/KOB-email-verification/build/tmp/kob-email-$version.zip
 
